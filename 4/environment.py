7277a80118e72e5b8b9ba86bffe565683187c295
Bruno <82845801+bmarroc@users.noreply.github.com>
2023-09-01 00:36:55 -0300

    Add files via upload

---
+++ 4/environment.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""Abstract environment base class for RL-Glue-py.
+"""
+
+from abc import abstractmethod
+
+class Environment:
+    """Implements the environment for an RLGlue environment
+
+    Note:
+        env_init, env_start, env_step, env_cleanup, and env_message are required
+        methods.
+    """
+
+    def __init__(self):
+        reward = None
+        observation = None
+        termination = None
+        self.reward_obs_term = (reward, observation, termination)
+
+    @abstractmethod
+    def env_init(self, env_info={}):
+        """Setup for the environment called when the experiment first starts.
+
+        Note:
+            Initialize a tuple with the reward, first state observation, boolean
+            indicating if it's terminal.
+        """
+
+    @abstractmethod
+    def env_start(self):
+        """The first method called when the experiment starts, called before the
+        agent starts.
+
+        Returns:
+            The first state observation from the environment.
+        """
+
+    @abstractmethod
+    def env_step(self, action):
+        """A step taken by the environment.
+
+        Args:
+            action: The action taken by the agent
+
+        Returns:
+            (float, state, Boolean): a tuple of the reward, state observation,
+                and boolean indicating if it's terminal.
+        """
+
+    @abstractmethod
+    def env_cleanup(self):
+        """Cleanup done after the environment ends"""
